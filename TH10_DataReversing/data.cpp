4bbe416061297c70b736e6d6a4698f006c1a8bb2
binvec <binvec@outlook.com>
2018-07-25 05:22:21 +0000

    修复一些注释错误

--- TH10_DataReversing/data.cpp
+++ TH10_DataReversing/data.cpp
@@ -176,8 +176,8 @@ void DataReader::GetItemsInfo(std::vector<Item>& items)
 			// type == 3 Faith Items 信仰点（绿点）
 			// type == 4 Large Power Items 大P点（红点）
 			// type == 5 Large Point Items 大得分点（带黄色边线的蓝点），由BOSS掉落
-			// type == 6 Life Items 续命点（紫点、1UP点）
-			// type == 7 Unknown
+			// type == 6 Unknown
+			// type == 7 Life Items 续命点（紫点、1UP点）
 			// type == 8 Unknown
 			// type == 9 Faith Items 信仰点（绿点），满灵力时由P点转化而来
 			// type == 10 Power Items P点（红点），由BOSS掉落
--- TH10_DataReversing/data.h
+++ TH10_DataReversing/data.h
@@ -73,11 +73,12 @@ struct Item : Object
 	// type == 3 Faith Items 信仰点（绿点）
 	// type == 4 Large Power Items 大P点（红点）
 	// type == 5 Large Point Items 大得分点（带黄色边线的蓝点），由BOSS掉落
-	// type == 6 Life Items 续命点（紫点、1UP点）
-	// type == 7 Unknown
+	// type == 6 Unknown
+	// type == 7 Life Items 续命点（紫点、1UP点）
 	// type == 8 Unknown
 	// type == 9 Faith Items 信仰点（绿点），满灵力时由P点转化而来
 	// type == 10 Power Items P点（红点），由BOSS掉落
+	// 点没有宽度和高度，自机靠近点时会自动收取，为了方便显示设定为6
 	int type;
 	Item(vec2 p, vec2 v, float w, float h, int type);
 };
